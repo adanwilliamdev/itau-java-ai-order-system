ea021787cf6e7b2b2a79b2a5b0266d03f16d81b7
Adan William <adan.william.dev@gmail.com>
2026-09-02 14:06:21 -0300

    correção

--- build.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-echo 📦 Compilando o projeto...
-echo ========================
-mvn clean install
-pause
--- run.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-echo 🚀 Iniciando o Sistema de Processamento de Pedidos...
-echo ==================================================
-mvn clean spring-boot:run
-pause
